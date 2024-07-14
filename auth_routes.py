6f2a0dd68f6b30877ad97eaff5ba852b62e8717b
Davronbek <atadjanovdavronbek863@gmail.com>
2024-07-14 12:42:08 +0500

    Login, token expire_time

--- auth_routes.py
+++ auth_routes.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, status
+import datetime
+
+from fastapi import APIRouter, status, Depends
+from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import HTTPException
 from models import User
-from schemas import SignUpModel
+from schemas import SignUpModel, Login
 from database import session, engine
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from fastapi_jwt_auth import AuthJWT
+from sqlalchemy import or_
 
 auth_router = APIRouter(
     prefix='/auth'
@@ -12,8 +16,13 @@ auth_router = APIRouter(
 
 session = session(bind=engine)
 
+
 @auth_router.get('/')
-async def welcome():
+async def welcome(Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
     return {'message': 'Bu signup sahifasi'}
 
 
@@ -56,6 +65,66 @@ async def signup(user: SignUpModel):
     return response_data
 
 
+@auth_router.post('/login', status_code=200)
+async def login(user: Login, Authorize: AuthJWT = Depends()):
+    # db_user = session.query(User).filter(User.username == user.username).first()
+
+    # query email or username
+    db_user = session.query(User).filter(
+        or_(
+            User.username == user.username_or_email,
+            User.email == user.username_or_email
+        )
+    ).first()
+
+    if db_user and check_password_hash(db_user.password, user.password):
+        access_lifetime = datetime.timedelta(minutes=60)
+        refresh_lifetime = datetime.timedelta(days=3)
+        access_token = Authorize.create_access_token(subject=db_user.username, expires_time=access_lifetime)
+        refresh_token = Authorize.create_refresh_token(subject=db_user.username, expires_time=refresh_lifetime)
+
+        token = {
+            "access": access_token,
+            "refresh": refresh_token
+        }
+
+        response = {
+            "success": True,
+            "code": 200,
+            "message": "User successfully login",
+            "data": token
+        }
+
+        return jsonable_encoder(response)
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid username or password")
 
 
+@auth_router.get('/login/refresh')
+async def refresh_token(Authorize: AuthJWT = Depends()):
+    try:
+        access_lifetime = datetime.timedelta(minutes=60)
+        Authorize.jwt_refresh_token_required()
+        current_user = Authorize.get_jwt_subject()
 
+        # Databasedan usernai filter oraqli topamiz
+
+        db_user = session.query(User).filter(User.username == current_user).first()
+        if db_user is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+        # access yaratamiz
+        new_access_token = Authorize.create_access_token(subject=db_user.username, expires_time=access_lifetime)
+
+        response = {
+            "success": True,
+            "code": 200,
+            "message": "New access token is created",
+            "data": {
+                "access_token": new_access_token
+            }
+        }
+
+        return response
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Refresh token")
